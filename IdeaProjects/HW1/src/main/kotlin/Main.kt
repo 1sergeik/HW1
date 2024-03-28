164da544c329cdf3211b89ad19c9bd58bb921ea0
1sergeik <1sergeik@gmail.com>
2024-03-28 19:36:16 +0200

    27/03/2024

--- IdeaProjects/HW1/src/main/kotlin/Main.kt
+++ IdeaProjects/HW1/src/main/kotlin/Main.kt
@@ -1,15 +1,14 @@
-fun main(){
-    println("Видите сумму перевода в рублях")
-    val amount_text = readLine() // зарплата в рублях текстом
-    val amount = amount_text!!.toInt() // зарплата в рублях числом
-    commission(0.75F, 35F, amount)
+const val minCommission = 35// минимальная комиссия константа, потому что используется часто
+const val commissionPercentsCardVisaMir = 0.75F // и тут тоже, дальше это значение нам нужно будет сохранить
+
+fun main() {
+    val amount = 5_087// зарплата в рублях числом, пока напрямую, у нас нет банка который делал бы перевод, а усложнять задачу не хотели бы
+    commissionVisaAndMir(amount) //вызов
 }
-fun commission(commission_percents:Float, min_commission:Float, transfer: Int){
 
-    val b =
-        if ((transfer * commission_percents)/100 > min_commission) transfer * commission_percents/100
-        else min_commission
-    val a = b.toInt()
-    println("Комисия при переводе составляет $a Руб")
+fun commissionVisaAndMir(transferAmount: Int) { //тут убрали лишние параметры, они не задаются, потому что они изначально установленные
 
+    val result = if ((transferAmount * commissionPercentsCardVisaMir) / 100 > minCommission) transferAmount * commissionPercentsCardVisaMir / 100 else minCommission
+    println("Комисия при переводе составляет $result Руб")
+    //А так хорошая работа, тут лишь недочеты, например: наименования неправильные, излишества были. Ну и значения было лучше перевести отдельно как константы.
 }
